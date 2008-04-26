1fc8d9001e1cd459fc33dda0484fe443c1c5c8da
michael <>
2008-04-26 08:31:32 +0000

    give up on tcp testing for now

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,35 +1,18 @@
 """
 Tests that do various complete logging runs like they might happen in a real
 application, and then check the output.
+
+There's a partial attempt at testing the TCP protocol using a dummy server
+in old SVN revisions, but this proved really complicated without implementing
+the protocol server side, so we're skipping that for now.
 """
 from __future__ import with_statement
 
 import os
 import subprocess
-import SocketServer
+import SocketServer, socket
 import StringIO
-
-TCP_PORT = 1234
-
-def setup_module(module):
-    class RequestHandler(SocketServer.BaseRequestHandler):
-        def setup(self):
-            self.request.send('SmartInspect Python Library Testrunner\r\n')
-            self.data = module.StringIO.StringIO()
-        def handle(self):
-            while True:
-                data = self.request.recv(1)
-                print data
-                if not data: break
-                self.data.write(data)
-                # we're sending way to many OKs, but it works and we don't
-                # have to speak the protocol this way.
-                self.request.send("OK")
-    module._server = SocketServer.ThreadingTCPServer(('', module.TCP_PORT), RequestHandler)
-    module._server.server_activate()
-
-def teardown_module(module):
-    module._server.server_close()
+import threading
 
 def test_examples(create_ref_data=False):
     """Run and test the example scripts in the subdirectory.
@@ -50,12 +33,11 @@ def test_examples(create_ref_data=False):
         # run each script in different variants
         for oname, option in [
                 ('mem', "mem()"),
-                #('tcp', "tcp(port=%d,timeout=3)"%TCP_PORT),
         ]:
             # run the test in the current variant
             p = subprocess.Popen([test_filepath, option], shell=True,
                                  stdout=subprocess.PIPE)
-            test_output, stderr = p.communicate()
+            test_output = p.communicate()[0]
 
             # path to the file containing the data to test against
             reference_filepath = \
@@ -95,12 +77,5 @@ def test_examples(create_ref_data=False):
         else:
             print "RESULT: all tests OK"
 
-
 if __name__ == '__main__':
-    # if this script is run directly, we generate new reference data
-    module = os.sys.modules.get(__name__)
-    setup_module(module)
-    try:
-        test_examples(create_ref_data=False)
-    finally:
-        teardown_module(module)
+    test_examples(create_ref_data=True)
